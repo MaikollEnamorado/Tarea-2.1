46d7599cb6dcf89b2887b7d67cd65b12dbe7faf9
Maikoll Enamorado <maikollenamorado73@gmail.com>
2024-07-21 15:40:46 -0600

    Inicio de proyecto

---
+++ Tarea_2/src/main/java/com/mycompany/tarea_2/Tarea_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea_2;
+
+/**
+ *
+ * @author LATITUDE 7390
+ */
+public class Tarea_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
